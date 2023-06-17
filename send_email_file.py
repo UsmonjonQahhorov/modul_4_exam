894b9fb85676d3542160a3d3f403f13dc82da536
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2023-06-18 01:48:34 +0500

    Lessons

--- send_email_file.py
+++ send_email_file.py
@@ -4,13 +4,15 @@ password_email = 'yeqzgnpuggwrpthv'
 sender = "qohhorovusmonjon@gmail.com"
 server = "smtp.gmail.com"
 port = 465
-reciever ="absaitovdev@gmail.com"
+reciever ="abdusalomabduvaliyev07@gmail.com"
 
 message = f"""
 Sender : {sender}
-Reciever : {reciever} 
+Reciever : {reciever}
 Github_link : https://github.com/UsmonjonQahhorov/modul_4_exam
-Dockerhub_link: qohhoroff/exam_bot:latest"""
+Dockerhub_link: qohhoroff/exam_bot:latest
+Docker.yml file githubda bor"""
 with smtplib.SMTP_SSL(server, port) as server:
     server.login(sender, password_email)
     server.sendmail(sender, reciever, message)
+
